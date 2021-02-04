6c3f562d0caf58a72e5d8c38e3ff8da8e92bfd79
Sergey Kovalev <xray@randoman.ru>
2021-02-04 22:49:13 +0700

    Added translation to local meter coordinates

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ import json
 import re
 import sys
 from os import walk
+import math
 import numpy as np
 from numpy.linalg import norm
 import matplotlib.path as mpltPath
@@ -11,10 +12,29 @@ import matplotlib.lines as mpltLines
 
 path = "../telemetry-data/2021-01-31/"
 
+class Transformer:
+    def __init__(self):
+        self.origin = [None, None]
+    def update_origin(self, c):
+        if self.origin[0] == None or c[0] < self.origin[0]:
+            self.origin[0] = c[0]
+
+        if self.origin[1] == None or c[1] < self.origin[1]:
+            self.origin[1] = c[1]
+
+
+    def to_meters(self, point):
+        point[0] = (point[0] - self.origin[0]) * 111320 * math.cos(math.radians(point[1]))
+        point[1] = (point[1] - self.origin[1]) * 111319
+
+transformer = Transformer()
+
 class Sectors:
+
     def __init__(self, file_path):
         self.polys = {}
         self.lines = {}
+        self.origin = [None, None]
         with open(file_path) as json_file:
             data = json.load(json_file)
             for feature in data['features']:
@@ -22,39 +42,52 @@ class Sectors:
                 otype = feature['geometry']['type']
 
                 coord3d = feature['geometry']['coordinates']
+
                 if otype.lower() == 'polygon':
                     coord2d = []
                     for c in coord3d[0]:
+                        transformer.update_origin(c)
                         coord2d.append([c[0], c[1]])
 
-                    self.polys[name] =  mpltPath.Path(coord2d)
+                    self.polys[name] =  coord2d
                     print("Loaded polygon {}".format(name))
                 if otype.lower() == 'linestring':
+
                     coord2d = []
                     for c in coord3d:
+                        transformer.update_origin(c)
                         coord2d.append([c[0], c[1]])
 
                     self.lines[name] = coord2d
+                    print(coord2d)
                     print("Loaded line {}".format(name))
 
-        self.track = self.polys['Track']
-        self.pre_finish = self.polys['PreFinish_Sector']
-        self.post_finish = self.polys['PostFinish_Sector']
-        self.opposite_marker = self.polys['Opposite_Marker']
-        self.pitlane = self.polys['Pitlane']
-        self.pitlane_gates = self.polys['Pitlane_Gates']
-        self.pitlane_entry = self.polys['Pitlane_entry']
-        self.pitlane_exit = self.polys['Pitlane_Exit']
-        self.paddock = self.polys['Paddock']
-        self.finish_line = self.lines['Finish_Line']
+        for n, poly in self.polys.items():
+            for point in poly:
+                transformer.to_meters(point)
 
-#        self.transitions = {}
-#        self.transitions[track] = [pitlane_entry, pitlane_exit]
-#        self.transitions[pitlane] = [pitlane_entry, pitlane_exit, pitlane_gates]
-#        self.transitions[paddock] = [pitlane_gates]
-#        self.transitions[pitlane_entry] = [track, pitlane]
-#        self.transitions[pitlane_exit] = [track, pitlane]
-#        self.transitions[pitlane_gates] = [paddock, pitlane]
+        for n, line in self.lines.items():
+            for point in line:
+                transformer.to_meters(point)
+
+        for n, poly in self.polys.items():
+            print(n)
+            for p in poly:
+                print(p)
+
+        print(self.origin)
+        # sys.exit(1)
+
+        self.track = mpltPath.Path(self.polys['Track'])
+        self.pre_finish = mpltPath.Path(self.polys['PreFinish_Sector'])
+        self.post_finish = mpltPath.Path(self.polys['PostFinish_Sector'])
+        self.opposite_marker = mpltPath.Path(self.polys['Opposite_Marker'])
+        self.pitlane = mpltPath.Path(self.polys['Pitlane'])
+        self.pitlane_gates = mpltPath.Path(self.polys['Pitlane_Gates'])
+        self.pitlane_entry = mpltPath.Path(self.polys['Pitlane_entry'])
+        self.pitlane_exit = mpltPath.Path(self.polys['Pitlane_Exit'])
+        self.paddock = mpltPath.Path(self.polys['Paddock'])
+        self.finish_line = self.lines['Finish_Line']
 
 sectors = Sectors("sectors.geojson")
 
@@ -105,6 +138,7 @@ for filename in files(path):
                     direction=float(m.group(12))
 
                 point = [lon, lat]
+                transformer.to_meters(point)
 
                 pre_finish = sectors.pre_finish.contains_points([point])
                 post_finish = sectors.post_finish.contains_points([point])
@@ -116,9 +150,10 @@ for filename in files(path):
                     cross = np.cross(finish_direction - finish_center, moving_point - finish_center)
                     sign = cross/np.abs(cross) 
                     dist = sign * norm(cross)/norm(finish_direction - finish_center)
+                    print("{} {}".format(hh*3600+mm*60+ss+ms/1000, dist))
 
-                if pre_finish:
-                    print("{}:{}:{}.{} pre_finish {}".format(hh, mm, ss, ms/100, dist))
+                # if pre_finish:
+                #     print("{}:{}:{}.{} pre_finish {}".format(hh, mm, ss, ms/100, dist))
 
-                if post_finish:
-                    print("{}:{}:{}.{} post_finish {}".format(hh, mm, ss, ms/100, dist))
+                # if post_finish:
+                #     print("{}:{}:{}.{} post_finish {}".format(hh, mm, ss, ms/100, dist))
