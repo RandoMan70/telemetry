e8fafb57a87ab146c95654db81890d6aae835fe7
Sergey Kovalev <skovalev@aura.com>
2021-03-11 17:34:36 +0700

    Muiltiple improvements

--- ubx-nmea-parser.py
+++ ubx-nmea-parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
 
 
 class SlidingBuffer:
@@ -41,11 +42,46 @@ def checksum_nmea(line: str):
     return cs
 
 
+def checksum_ubx(data):
+    CK_A = 0
+    CK_B = 0
+    for b in data:
+        CK_A = (CK_A + b) % 256
+        CK_B = (CK_B + CK_A) % 256
+
+    return bytes([CK_A, CK_B])
+
+
+def retrieve_ubx(buffer: SlidingBuffer):
+    start = buffer.lookup(2)
+
+    if start != b'\xb5\x62':
+        return None
+
+    header = buffer.lookup(6)
+    payload_length = (header[5] << 8) + header[4]
+    frame_length = payload_length + 8
+
+    frame = buffer.lookup(frame_length)
+    if len(frame) != frame_length:
+        print("Too short ubx frame", len(frame), frame_length, payload_length)
+        return None
+
+    frame_checksum = frame[-2:]
+    payload_checksum = checksum_ubx(frame[2:-2])
+
+    if frame_checksum != payload_checksum:
+        print("Invalid ubx checksum", frame_checksum, payload_checksum)
+        return None
+
+    buffer.commit(frame_length)
+    return frame
+
+
 def retrieve_nmea(buffer: SlidingBuffer):
     start = buffer.lookup(1)
 
     if start != b'$':
-        print("Invalid nmea sync byte")
         return None
 
     frame = buffer.lookup(128)
@@ -74,27 +110,86 @@ def retrieve_nmea(buffer: SlidingBuffer):
         return None
 
     buffer.commit(p)
-
     return line
 
+
+def nmea_extract_timestamp(line):
+    cmd = line[:6]
+    if not cmd.startswith('$G') or not cmd.endswith('RMC'):
+        return None, None
+
+    parts=line.split(',')
+    return parts[1], parts[9]
+
+
+class OutputDir:
+    def __init__(self, path: str):
+        self.__path = path
+        self.__current_suffix: Optional[str] = None
+        self.__current_file: Optional[file] = None
+
+    def get_file(self, suffix: str):
+        assert suffix is not None
+        if self.__current_suffix == suffix:
+            return self.__current_file
+
+        if self.__current_file is not None:
+            self.__current_file.close()
+
+        self.__current_suffix = suffix
+        self.__current_file = open(self.__path + '/' + suffix, 'wb')
+
+        return self.__current_file
 
 def __main__():
     parser = argparse.ArgumentParser(description="Process ubx+nmea dumps")
     parser.add_argument("--infile", type=str, required=True)
+    parser.add_argument("--outfile", type=str)
     parser.add_argument("--outdir", type=str)
+    parser.add_argument("--verbose", type=bool)
     args = parser.parse_args()
+    suffix = None
 
     with open(args.infile, 'rb') as f:
         buffer = SlidingBuffer(f)
+        outfile = None
+        outdir = None
+
+        if args.outfile is not None:
+            outfile = open(args.outfile, 'wb')
+
+        if args.outdir is not None:
+            outdir = OutputDir(args.outdir)
 
         while not buffer.eof():
-            nmea_sentence = None
             ubx_sentence = None
 
-            if buffer.lookup(1) == b'$':
-                nmea_sentence = retrieve_nmea(buffer)
+            nmea_sentence = retrieve_nmea(buffer)
+            if nmea_sentence is not None:
+                time, date = nmea_extract_timestamp(nmea_sentence)
+                if time is not None and date is not None and time != '' and date != '':
+                    suffix = '{}-{}-{}_{}-{:02d}'.format(date[4:6], date[2:4], date[:2], time[:2], int(int(time[2:4]) / 5) * 5)
 
-            if nmea_sentence is not None or ubx_sentence is not None:
+                if outfile is not None:
+                    outfile.write(nmea_sentence.encode())
+
+                if outdir is not None and suffix is not None:
+                    outdir.get_file(suffix).write(nmea_sentence.encode())
+
+                if args.verbose:
+                    print(nmea_sentence.strip())
+                continue
+
+            ubx_sentence = retrieve_ubx(buffer)
+            if ubx_sentence is not None:
+                if outfile is not None:
+                    outfile.write(ubx_sentence)
+
+                if outdir is not None and suffix is not None:
+                    outdir.get_file(suffix).write(ubx_sentence)
+
+                if args.verbose:
+                    print("<UBX>")
                 continue
 
             print("Sliding one byte forward for next try")
